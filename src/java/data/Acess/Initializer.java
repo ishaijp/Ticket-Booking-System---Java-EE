5913c8f599e5474294467b53ca4febf6b3cb6be8
Ishrat Jahan <50982182+ishaijp@users.noreply.github.com>
2024-04-29 15:34:47 +0600

    Add files via upload

---
+++ src/java/data/Acess/Initializer.java
@@ -0,0 +1,9 @@
+package data.Acess;
+
+public interface Initializer 
+{
+    String DRIVER ="com.mysql.jdbc.Driver";
+    String DB = "jdbc:mysql://localhost:3306/project";
+    String USER = "isha";
+    String PASS = "1234";
+}
